ba1ca2ac06800ceabf1aa3bdda360618c145c44a
Dajuan Mcdonald <flopwiki@gmail.com>
2020-05-17 19:31:56 -0400

    implemented registration endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,19 @@
-const router = require('express').Router();
+const router = require('express');
+const bcrypt = require('bcryptjs')
+const db = require('../database/dbConfig');
+const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
   // implement registration
+  let user = req.body;
+
+  const hash = bcrypt.hash(user.password, 12)
+  user.password = hash;
+
+  const user = await db('auth')
+    .insert(user)
+    
+    res.status(201).json(user)
 });
 
 router.post('/login', (req, res) => {
