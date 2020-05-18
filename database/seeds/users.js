faa1844fe458cbf2e3bba809729b67a02cc5cb27
Dajuan Mcdonald <flopwiki@gmail.com>
2020-05-17 21:25:45 -0400

    fixed seeded table

--- database/seeds/users.js
+++ database/seeds/users.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('users').insert([
         {username: 'guest_1', password: 'notNull'},
         {username: 'guest_2', password: 'null'},
         {username: 'guest_3', password: 'NaN'}
