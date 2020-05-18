1f1aaf6b28b56083bce26c93ea5d7d462a9f8c93
Dajuan Mcdonald <flopwiki@gmail.com>
2020-05-17 20:13:00 -0400

    wired up login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,19 +1,29 @@
-const router = require('express');
+const router = require('express').Router();
 const bcrypt = require('bcryptjs')
 const db = require('../database/dbConfig');
-const router = express.Router();
 
-router.post('/register', async (req, res) => {
+
+router.get('/register', (req, res) => {
+
+  res.send('<h1>Welcome to API</h1>')
+
   // implement registration
-  let user = req.body;
+  // try {
 
-  const hash = bcrypt.hash(user.password, 12)
-  user.password = hash;
+  //   let user = req.body;
+  
+  //   const hash = bcrypt.hash(username.password, 12)
+  //   user.username.password = hash;
+  
+  //   const newUser = await db('users')
+  //     .insert(newUser)
+      
+  //     res.status(201).json(newUser)
 
-  const user = await db('auth')
-    .insert(user)
-    
-    res.status(201).json(user)
+  // } catch(err) {
+  //   res.sendStatus(err)
+  // }
+
 });
 
 router.post('/login', (req, res) => {
