3f5748f904f7caa01773c0125d661408d19bc3fd
Dajuan Mcdonald <flopwiki@gmail.com>
2020-05-17 21:40:35 -0400

    fixed middware.js

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,27 +1,20 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
 const jwt = require('jsonwebtoken');
-const cookie = require('../session/secrets.js');
-
+const cookie = require('../session/secrets');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (authorization)  { jwt.verify(authorization, cookie.jwtSecret, 
-    function(err, decoded) {
-      if (err) {
-        res.status(401).json({ you: 'shall not pass!' });
-      
-          
-      } else {
-        req.token = decoded;
-        next()
-      }
-
-    })
+  if (authorization) {
+    jwt.verify(authorization, cookie.jwtSecret, 
+      function(err, decoded) {
+        if(err) {
+          res.status(401).json({you: 'Shall not pass!'})
+        } else {
+          req.token = decoded;
+          next()
+        }
+      })
   } else {
-    res.status(500).json({message: 'Server error...we are working on it ! '})
+    res.status(400).json({message: 'Login failed'})
   }
-};
+}
